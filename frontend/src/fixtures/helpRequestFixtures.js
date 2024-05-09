10571d320feedaee7be8301af590a87d70a87903
Aiden <aidenpham@ucsb.edu>
2024-05-09 13:56:12 -0700

    ap - added fixtures for helprequest

---
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -0,0 +1,46 @@
+const helpRequestFixtures = {
+    oneRequest: 
+    [
+        {
+            "id": 1,
+            "requesterEmail": "aidenpham@ucsb.edu",
+            "teamId": "s24-4pm-1",
+            "tableOrBreakoutRoom": "1",
+            "requestTime": "2022-04-03T12:00:00",
+            "explanation": "Swagger",
+            "solved": false, 
+        },
+    ],
+    threeRequests:
+    [
+        {
+            "id": 2,
+            "requesterEmail": "cgaucho@ucsb.edu",
+            "teamId": "s24-4pm-2",
+            "tableOrBreakoutRoom": "13",
+            "requestTime": "2022-04-04T12:00:00",
+            "explanation": "Dokku",
+            "solved": false, 
+        },
+        {
+            "id": 3,
+            "requesterEmail": "cyang@ucsb.edu",
+            "teamId": "s24-5pm-1",
+            "tableOrBreakoutRoom": "9",
+            "requestTime": "2022-05-03T12:00:00",
+            "explanation": "Postgres",
+            "solved": false, 
+        },
+        {
+            "id": 4,
+            "requesterEmail": "phtcon@ucsb.edu",
+            "teamId": "s24-5pm-5",
+            "tableOrBreakoutRoom": "4",
+            "requestTime": "2022-04-10T12:00:00",
+            "explanation": "ReactJS",
+            "solved": false, 
+        },
+    ]
+};
+
+export { helpRequestFixtures };
